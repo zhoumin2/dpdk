6c16650d20aa24daed59caed75acc2af28bf2eee
Min Zhou <zhoumin@loongson.cn>
2022-05-20 08:57:54 +0800

    eal/loongarch: add power operations for LoongArch
    This patch adds power operations for LoongArch architecture. In
    fact, these operations are temporarily not supported on LoongArch.
    
    Signed-off-by: Min Zhou <zhoumin@loongson.cn>

---
+++ lib/eal/loongarch/include/rte_power_intrinsics.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2022 Loongson Technology Corporation Limited
+ */
+
+#ifndef _RTE_POWER_INTRINSIC_LOONGARCH_H_
+#define _RTE_POWER_INTRINSIC_LOONGARCH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <rte_common.h>
+
+#include "generic/rte_power_intrinsics.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_POWER_INTRINSIC_LOONGARCH_H_ */
