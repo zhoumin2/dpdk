26f2c204b63673c68a580c935af9906748501ae6
Min Zhou <zhoumin@loongson.cn>
2022-05-20 08:52:28 +0800

    eal/loongarch: add ticketlock operations for LoongArch
    This patch adds ticketlock operations for LoongArch architecture.
    Let it uses generic ticketlock implementation.
    
    Signed-off-by: Min Zhou <zhoumin@loongson.cn>

---
+++ lib/eal/loongarch/include/rte_ticketlock.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2022 Loongson Technology Corporation Limited
+ */
+
+#ifndef _RTE_TICKETLOCK_LOONGARCH_H_
+#define _RTE_TICKETLOCK_LOONGARCH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "generic/rte_ticketlock.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_TICKETLOCK_LOONGARCH_H_ */
